061436f2c2b35bfca21feaa1fd082ff70a295472
Kohei Asai <me@axross.io>
2019-02-09 21:49:54 -0800

    Deoptimize binary search and make it readable

--- src/searching/binary-search.ts
+++ src/searching/binary-search.ts
@@ -11,35 +11,36 @@ function binarySearch<Value>({
   compare: (a: Value, b: Value) => number;
   onComparison?: (step: SearchComparison<Value>) => void;
 }): number {
-  let searchRangeStart = 0;
-  let searchRangeEnd = array.length - 1;
-  let searchRangeMiddle: number;
-  let foundTargetIndex: number = -1;
+  let searchRange = new Range(0, array.length - 1);
 
-  while (searchRangeStart <= searchRangeEnd) {
-    searchRangeMiddle = Math.floor((searchRangeStart + searchRangeEnd) / 2);
-
-    const picked = array[searchRangeMiddle];
+  while (searchRange.start <= searchRange.end) {
+    const picked = array[searchRange.middle];
 
     if (picked === target) {
-      foundTargetIndex = searchRangeMiddle;
-
-      break;
+      return searchRange.middle;
     }
 
     onComparison({
-      index: searchRangeMiddle,
+      index: searchRange.middle,
       value: picked
     });
 
     if (compare(picked, target) <= -1) {
-      searchRangeStart = searchRangeMiddle + 1;
+      searchRange.start = searchRange.middle + 1;
     } else {
-      searchRangeEnd = searchRangeMiddle - 1;
+      searchRange.end = searchRange.middle - 1;
     }
   }
 
-  return foundTargetIndex;
+  return -1;
+}
+
+class Range {
+  constructor(public start: number, public end: number) {}
+
+  get middle(): number {
+    return Math.floor((this.start + this.end) / 2);
+  }
 }
 
 export default binarySearch;
