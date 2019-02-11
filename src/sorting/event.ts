b1ac428662432448d903ad69c7b36ef2cd6aaa8f
Kohei Asai <me@axross.io>
2019-02-10 22:28:08 -0800

    Delete unnecessary events

--- src/sorting/event.ts
+++ src/sorting/event.ts
@@ -21,23 +21,3 @@ export interface SortSwap<Element> {
     value: Element;
   };
 }
-
-export interface SortMerge<Value> {
-  from: {
-    a: Value[];
-    b: Value[];
-  };
-  into: Value[];
-}
-
-export type OnMerge<Value> = (merge: SortMerge<Value>) => void;
-
-export interface SortSplit<Value> {
-  from: Value[];
-  into: {
-    a: Value[];
-    b: Value[];
-  };
-}
-
-export type OnSplit<Value> = (split: SortSplit<Value>) => void;
